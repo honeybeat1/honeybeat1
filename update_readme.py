bd7bfa1ec2600a4132d37d95acbf46a07f02be7b
Dahyun Chung ☁️ <40647396+honeybeat1@users.noreply.github.com>
2022-05-04 00:53:14 +0900

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -4,12 +4,13 @@ blog_rss_url = 'https://v2.velog.io/rss/honeybeat1'
 rss_feed = feedparser.parse(blog_rss_url)
 MAX_POST_NUM = 10
 latest_blog_post_list = ""
+MAX_POST_NUM = 10
 
 for idx, feed in enumerate(rss_feed['entries']):
     if idx > MAX_POST_NUM:
         break
     feed_date = feed['published_parsed']
-    latest_blog_post_list += f"{feed_date.tm_year}/{feed_date.tm_mon}/{feed_date.tm_mday} - {feed['title']}]({feed['link']}) <br>\n"
+    latest_blog_post_list += f"[{feed_date.tm_year}/{feed_date.tm_mon}/{feed_date.tm_mday} - {feed['title']}]({feed['link']}) <br>\n"
 
 markdown_txt = """기본으로 변하지 않을 README.md 값"""
 readme_txt = f"{markdown_txt}{latest_blog_post_list}"
